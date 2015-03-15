0a8a8b68e125e32c1177f6d2243b4298ed380b31
LennertVanmunster <lennert.vanmunster@student.kuleuven.be>
2015-03-15 13:06:50 +0100

    kleine aanpassing

--- OGP1415-JumpingAlien/src/jumpingalien/model/Mazub.java
+++ OGP1415-JumpingAlien/src/jumpingalien/model/Mazub.java
@@ -1063,7 +1063,7 @@ public class Mazub {
 	 * Return the current Sprite of this Mazub.
 	 */
 	public Sprite getCurrentSprite(){
-		int newSpriteIndex=0;
+		int newSpriteIndex=Integer.MAX_VALUE;
 		if (!this.isMovingHorizontally() && this.isDucking()==false && this.getTimeSinceEndMove()>1)
 			newSpriteIndex=0;
 		else if (!this.isMovingHorizontally() && this.isDucking()==true && this.getTimeSinceEndMove()>1)
