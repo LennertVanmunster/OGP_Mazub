a0feb25fe536e6d7c9a5817fa5050fa9dd1adf74
Reteipvd <reteipvd@gmail.com>
2015-05-18 20:44:21 +0200

    All tests voor part 2

--- OGP1415-JumpingAlien/tests/jumpingalien/part2/tests/AllTests.java
+++ OGP1415-JumpingAlien/tests/jumpingalien/part2/tests/AllTests.java
@@ -1,6 +1,7 @@
 package jumpingalien.part2.tests;
 
 
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { GameObjectTest.class, MazubTest.class,
     PartialFacadeTest.class, PlantTest.class, SchoolTest.class,
